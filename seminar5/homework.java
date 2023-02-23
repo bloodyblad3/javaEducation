a3f95e9bb9916dce9a4c643d0ce01551156daa1d
externalcharm <freesxsad23@mail.ru>
2023-02-24 00:54:18 +0300

    homework

---
+++ seminar5/homework.java
@@ -0,0 +1,73 @@
+package seminar5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class homework1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = getString(sc);
+        System.out.printf("%s > %s", text, checkBrackets(text));
+    }
+
+    private static String getString(Scanner sc) {
+        System.out.println("Для работы введите скобочки, например: []{}((<>))");
+        return sc.nextLine();
+    }
+
+    private static boolean isInArray(char symb, char[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException(); 
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == symb) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int indexOf(char symb, char[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == symb) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private static boolean checkBrackets(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Stack<Character> stack = new Stack<Character>();
+        char[] openBrackets = new char[] { '{', '[', '(', '<' };
+        char[] closeBrackets = new char[] { '}', ']', ')', '>'};
+
+        for (int i = 0; i < str.length(); i++) {
+            char symb = str.charAt(i);
+            if (isInArray(symb, openBrackets)) {
+                stack.push(symb);
+            } else {
+                int index = indexOf(symb, closeBrackets);
+                if (index != -1) {
+                    if (stack.empty()) {
+                        return false;
+                    }
+                    if (stack.pop() != openBrackets[index]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
